42d8e870f171d9620515c7529419ddb5934fdac7
Ragnar-F <ragnarfrost@gmail.com>
2014-04-08 11:02:32 +0200

    Weitere Sonderzeichen im Anker entfernen

--- static/content.js
+++ static/content.js
@@ -297,8 +297,9 @@ $(document).ready(function() {
   $('h1, h2, h3, h4, h5, h6').each(function(index) {
     if(!$(this).attr('id')) // if id anchor not exist, create one
     {
+      
       var str = $(this).text().replace(/\s/g, '_'); // replace spaces with _
-      var str = str.replace(/(\(|\)|:|\.|,|#|\[|\]|\/|\{|\}|&|=)/g, ''); // remove special chars
+      var str = str.replace(/[():.,#\[\]\/{}&="|?!]/g, ''); // remove special chars
       if($('#' + str).length) // if new id anchor exist already, set it to a unique one
         $(this).attr('id', str + '_' + index);
       else
