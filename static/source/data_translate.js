131cbb85db7a1c1b42c80a97c5d17c324ed8a560
Ragnar-F <ragnarfrost@gmail.com>
2015-01-25 20:36:50 +0100

    v1.1.19.02

---
+++ static/source/data_translate.js
@@ -0,0 +1,11 @@
+translate = {
+  hdSearchTxt: "Suchbegriff eingeben ...",
+  hdSearchBtn: "Suchen",
+  hdSearchLnk: "'https://www.google.com/search?sitesearch=' + location.host + '&amp;q=' + query",
+  sbContent: "Inhalt",
+  sbIndex: "Index",
+  ftLicense: "Lizenz:",
+  ftExtra: " | Übersetzung: Harald Bootz",
+  cdSelectBtn: "Markieren",
+  cdDownloadBtn: "Download"
+};
