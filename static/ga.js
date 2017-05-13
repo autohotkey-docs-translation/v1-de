17a130ee2119f7e284790068fcdf8e813ffb8c4c
Ragnar-F <ragnarfrost@gmail.com>
2017-05-13 23:52:03 +0200

    insertBefore -> appendChild

--- static/ga.js
+++ static/ga.js
@@ -1,7 +1,7 @@
 if ('file:mk:ms-its:'.indexOf(location.protocol) == -1) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.appendChild(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-28224374-1', 'auto');
