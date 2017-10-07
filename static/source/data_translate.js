8e6c53f265c7d5c69c3422bafbe8daffe8fca93a
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2017-10-07 15:00:32 +0200

    Diverse Sidebar-Änderungen: * Fix build_search * Add button for editing the page

--- static/source/data_translate.js
+++ static/source/data_translate.js
@@ -13,6 +13,8 @@ translateData = {
   "v1":true,
   "Change AHK version":"AHK-Version wechseln",
   "https://lexikos.github.io/v2/docs/":"https://ahkde.github.io/v2/docs/",
+  "Edit page on GitHub":"Seite auf GitHub bearbeiten",
+  "https://github.com/Lexikos/AutoHotkey_L-Docs/edit/master/docs/":"https://github.com/ahkde/docs/edit/master/",
   "Go back":"Eine Seite zurück",
   "Go forward":"Eine Seite vor",
   "Change font size":"Schriftgröße ändern",
