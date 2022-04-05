5fdbc82bb70201a4e9a69da622bb8a7019646cf9
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2022-04-05 11:12:54 +0200

    Add Google Analytics 4

---
+++ static/ga4.js
@@ -0,0 +1,13 @@
+// Global site tag (gtag.js) - Google Analytics
+
+var id = 'G-HT22RDW7DT';
+
+var script = document.createElement('script');
+script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=' + id);
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', id);
